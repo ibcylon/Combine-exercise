246ebbca08f52b113bd5778c10bc811877f7e0e5
kangho lee <ibcylon@naver.com>
2024-01-30 11:50:15 +0900

    Fix: Publisher to Future

--- Coding_test/DataLayer/Sources/DataLayer/Data/Service/CurrencyService/CurrencyRepository.swift
+++ Coding_test/DataLayer/Sources/DataLayer/Data/Service/CurrencyService/CurrencyRepository.swift
@@ -13,6 +13,7 @@ import DomainLayer
 public final class CurrencyRepository {
   private let httpClient: HTTPClient
   private let token: String
+  private var cacellable = Set<AnyCancellable>()
 
   public init(httpClient: HTTPClient, token: String) {
     self.httpClient = httpClient
@@ -24,21 +25,32 @@ extension CurrencyRepository: CurrencyRepositoryInterface {
   public func realtimeRequest(currencies: [String], source: String) -> AnyPublisher<CurrencyInfo, Error> {
     let query = RealtimeReq(accessKey: self.token, currencies: currencies, source: source)
     let endpoint = EndPoint(target: .realtime(request: query))
-
-    return httpClient.request(request: endpoint.toRequest())
-      .tryMap({ (data, response) in
-        let decoder = JSONDecoder()
-        guard let dto = try? decoder.decode(CurrencyResponse.self, from: data) else {
-          do {
-            let errorResponse = try decoder.decode(ErrorResponse.self, from: data)
-            throw DomainError.networkError(errorResponse.error)
-          } catch let error as DecodingError {
-            throw DomainError.decodedError(error)
+    
+    return Future { promise in
+      self.httpClient.request(request: endpoint.toRequest())
+        .tryMap({ (data, response) in
+          let decoder = JSONDecoder()
+          guard let dto = try? decoder.decode(CurrencyResponse.self, from: data) else {
+            do {
+              let errorResponse = try decoder.decode(ErrorResponse.self, from: data)
+              throw DomainError.networkError(errorResponse.error)
+            } catch let error as DecodingError {
+              throw DomainError.decodedError(error)
+            }
           }
+          return dto.toDomain()
+        })
+        .sink { completion in
+          if case .failure(let error) = completion {
+            promise(.failure(error))
+          }
+
+        } receiveValue: { output in
+          print(output)
+          promise(.success(output))
         }
-        return dto.toDomain()
-      })
-      .eraseToAnyPublisher()
+        .store(in: &self.cacellable)
+    }.eraseToAnyPublisher()
   }
   
   public func historicalRequest(date: Date, source: CurrencyEntity) -> AnyPublisher<CurrencyInfo, Error> {
